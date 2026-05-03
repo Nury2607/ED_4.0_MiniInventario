d00a57bc253fb42d78db5576249d58809a79b180
Nury <nschavida@gmail.com>
2026-05-03 17:43:13 +0200

    Incorpora mejora Main en rama funcional

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("MiniInventario iniciado correctamente.");
-        System.out.println("Cambio realizado para seguna parte practica");
+        System.out.println("Mejora para tarea 2 Git");
     }
 }
 
